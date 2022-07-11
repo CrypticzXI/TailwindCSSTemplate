e7454879856daa223fade653d797878d0ad656bc
CrypticzXI <CrypticzXI@gmail.com>
2022-07-11 01:28:37 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  //presets: [require("./preset-app")],
+  purge: ["./public/**/*.html"],
+  darkMode: false,
+  mode: "jit",
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
